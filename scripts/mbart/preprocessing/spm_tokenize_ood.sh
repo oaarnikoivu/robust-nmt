955107e071f3553f334676e8d1087b86c3719a51
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2021-08-11 19:38:02 +0200

    clean

--- scripts/mbart/preprocessing/spm_tokenize_ood.sh
+++ scripts/mbart/preprocessing/spm_tokenize_ood.sh
@@ -17,23 +17,23 @@ MODEL=./mbart.cc25/sentence.bpe.model
 
 for domain in law medical religion; do
 	echo "Tokenizing for the $domain domain:"
-
+	
 	DATA=$data/out_domain/$domain
-	OUTDIR=$base/ft/$domain 
-
+	OUTDIR=$base/ft/$domain
+	
 	mkdir -p $OUTDIR
-
-    # Deduplicate ood test set 
+	
+	# Deduplicate ood test set
 	test_src_file=$DATA/test.$SRC
 	test_tgt_file=$DATA/test.$TGT
-
+	
 	paste $test_src_file $test_tgt_file | awk '!x[$0]++' > $DATA/test.dedup
-
-	cut -f1 $DATA/test.dedup > $DATA/test.deduped.$SRC 
+	
+	cut -f1 $DATA/test.dedup > $DATA/test.deduped.$SRC
 	cut -f2 $DATA/test.dedup > $DATA/test.deduped.$TGT
-
+	
 	TEST=test.deduped
-
+	
 	python $spm/spm_encode.py --model=${MODEL} < ${DATA}/${TEST}.${SRC} > ${OUTDIR}/${TEST}.spm.${SRC} &
 	python $spm/spm_encode.py --model=${MODEL} < ${DATA}/${TEST}.${TGT} > ${OUTDIR}/${TEST}.spm.${TGT} &
 	
