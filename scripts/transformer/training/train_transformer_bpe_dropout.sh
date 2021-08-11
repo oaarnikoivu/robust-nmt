370aea17e430d68fae65dc0cf07308c1dc6c0c65
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2021-08-11 18:38:53 +0200

    align

--- scripts/transformer/training/train_transformer_bpe_dropout.sh
+++ scripts/transformer/training/train_transformer_bpe_dropout.sh
@@ -11,13 +11,13 @@ fairseq-train $data_dir \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 0.001 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.1 --weight-decay 0.0001 \
-	--update-freq 8 \
+    --update-freq 8 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 12288 \
     --patience $patience \
     --seed $seed \
-	--ddp-backend no_c10d \
-	--fp16 \
+    --ddp-backend no_c10d \
+    --fp16 \
     --eval-bleu \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-detok moses \
