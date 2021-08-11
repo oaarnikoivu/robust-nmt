88fcf95de3276d9783d835c17ec5286e5772bb4c
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2021-08-11 18:45:28 +0200

    update

--- scripts/transformer/evaluation/eval_id_transformer.sh
+++ scripts/transformer/evaluation/eval_id_transformer.sh
@@ -13,30 +13,31 @@ size=$2
 bin_dir=$base/data-bin/$experiment/$src-$tgt 
 
 for seed in 1 2 3; do
-	echo "Evaluating at seed $seed:"
-
-    # If evaluating the baseline change "transformer_optim" to "transformer_baseline" below! 
-	checkpoint_dir=$base/checkpoints/transformer_optim/$experiment/$seed 
-    result_dir=$base/translations/transformer_optim/$size/$seed 
-
-    # checkpoint_dir=$base/checkpoints/transformer_baseline/$experiment/$seed 
-    # result_dir=$base/translations/transformer_baseline/$size/$seed 
-
-	fairseq-generate $bin_dir \
+    echo "Evaluating at seed $seed:"
+    
+    # If evaluating the baseline change "transformer_optim" to "transformer_baseline" below!
+    checkpoint_dir=$base/checkpoints/transformer_optim/$experiment/$seed
+    result_dir=$base/translations/transformer_optim/$size/$seed
+    
+    # checkpoint_dir=$base/checkpoints/transformer_baseline/$experiment/$seed
+    # result_dir=$base/translations/transformer_baseline/$size/$seed
+    
+    fairseq-generate $bin_dir \
         --source-lang $src --target-lang $tgt \
         --path $checkpoint_dir/checkpoint_best.pt \
-		--beam 5 \
+        --beam 5 \
         --remove-bpe \
-		--sacrebleu \
+        --sacrebleu \
         --results-path $result_dir
-
-	grep ^H $result_dir/generate-test.txt | cut -f3- | sacremoses detruecase | sacremoses detokenize > $result_dir/gen.out.detok.sys
-	grep ^T $result_dir/generate-test.txt | cut -f2- | sacremoses detruecase | sacremoses detokenize > $result_dir/gen.out.detok.ref
-
+        
+    grep ^H $result_dir/generate-test.txt | cut -f3- | sacremoses detruecase | sacremoses detokenize > $result_dir/gen.out.detok.sys
+    grep ^T $result_dir/generate-test.txt | cut -f2- | sacremoses detruecase | sacremoses detokenize > $result_dir/gen.out.detok.ref
+    
     # Evaluate against reference using sacreBLEU
-	cat $result_dir/gen.out.detok.sys | sacrebleu $result_dir/gen.out.detok.ref
+    cat $result_dir/gen.out.detok.sys | sacrebleu $result_dir/gen.out.detok.ref
+    
+    echo ""
 
-	echo ""
 done
 
 echo "Done."
