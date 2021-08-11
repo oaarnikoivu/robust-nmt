f4c39c97bcadc9d6df4ef35e23502f708a1ac6a7
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2021-08-11 18:49:25 +0200

    .

--- scripts/mbart/eval_ood.sh
+++ scripts/mbart/eval_ood.sh
@@ -11,29 +11,30 @@ langs=ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk
 for domain in law medical religion; do
 	for seed in 222 223 224 225; do
 		echo "Evaluating the $domain domain at seed $seed:"
-
+		
 		DATA_PATH=$base/mbart_processed/$size/$domain
 		checkpoint_dir=$base/checkpoints/mbart/$seed/$size
-
-        outdir=$base/translations/mbart/$seed/$domain 
-
+		
+		outdir=$base/translations/mbart/$seed/$domain 
+		
 		mkdir -p $outdir
-
+		
 		fairseq-generate $DATA_PATH \
-  			--path $checkpoint_dir/checkpoint_best.pt \
-  			--task translation_from_pretrained_bart \
-  			--gen-subset test \
-  			-t fi_FI -s en_XX \
-  			--remove-bpe 'sentencepiece' \
-  			--sacrebleu \
-  			--langs $langs > $outdir/en_fi
-
+			--path $checkpoint_dir/checkpoint_best.pt \
+			--task translation_from_pretrained_bart \
+			--gen-subset test \
+			-t fi_FI -s en_XX \
+			--remove-bpe 'sentencepiece' \
+			--sacrebleu \
+			--langs $langs > $outdir/en_fi
+			
 		cat $outdir/en_fi | grep -P "^H" | sort -V | cut -f3- | sed 's/\[fi_FI\]//g' > $outdir/gen.out.detok.sys
 		cat $outdir/en_fi | grep -P "^T" | sort -V | cut -f2- | sed 's/\[fi_FI\]//g' > $outdir/gen.out.detok.ref
-
+		
 		sacrebleu -tok 'none' -s 'none' $outdir/gen.out.detok.ref < $outdir/gen.out.detok.sys
-
+		
 		echo ""
+	
 	done
 done
 
